4a09723887fbc9a52e07fc512d50442496c27831
Cyphall <cyphallbraus@gmail.com>
2021-03-12 14:52:59 +0100

    Refactor: Reformatted Portal and PortalTraveller and removed most warnings

--- Assets/Scripts/Player/PortalTraveller.cs
+++ Assets/Scripts/Player/PortalTraveller.cs
@@ -1,66 +1,87 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class PortalTraveller : MonoBehaviour {
+public class PortalTraveller : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject _graphicsObject;
+	
+	public GameObject GraphicsObject => _graphicsObject;
+	public GameObject GraphicsClone { get; set; }
+	
+	public Vector3 PreviousOffsetFromPortal { get; set; }
 
-    public GameObject graphicsObject;
-    public GameObject graphicsClone { get; set; }
-    public Vector3 previousOffsetFromPortal { get; set; }
+	public Material[] OriginalMaterials { get; set; }
+	public Material[] CloneMaterials { get; set; }
+	
+	// Shader uniforms
+	private static readonly int SliceNormal = Shader.PropertyToID("sliceNormal");
+	private static readonly int SliceOffsetDst = Shader.PropertyToID("sliceOffsetDst");
 
-    public Material[] originalMaterials { get; set; }
-    public Material[] cloneMaterials { get; set; }
+	public virtual void Teleport(Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot)
+	{
+		transform.position = pos;
+		transform.rotation = rot;
+	}
 
-    public virtual void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
-        transform.position = pos;
-        transform.rotation = rot;
-    }
+	// Called when first touches portal
+	public virtual void EnterPortalThreshold()
+	{
+		if (Equals(_graphicsObject, null)) return;
 
-    // Called when first touches portal
-    public virtual void EnterPortalThreshold ()
-    {
-        if (Equals(graphicsObject, null)) return;
-        
-        if (graphicsClone == null) {
-            graphicsClone = Instantiate(graphicsObject, graphicsObject.transform.parent);
-            graphicsClone.transform.localScale = graphicsObject.transform.localScale;
-            originalMaterials = GetMaterials (graphicsObject);
-            cloneMaterials = GetMaterials (graphicsClone);
-        } else {
-            graphicsClone.SetActive (true);
-        }
-    }
+		if (GraphicsClone == null)
+		{
+			GraphicsClone = Instantiate(_graphicsObject, _graphicsObject.transform.parent);
+			GraphicsClone.transform.localScale = _graphicsObject.transform.localScale;
+			OriginalMaterials = GetMaterials(_graphicsObject);
+			CloneMaterials = GetMaterials(GraphicsClone);
+		}
+		else
+		{
+			GraphicsClone.SetActive(true);
+		}
+	}
 
-    // Called once no longer touching portal (excluding when teleporting)
-    public virtual void ExitPortalThreshold ()
-    {
-        if (Equals(graphicsObject, null)) return;
-        
-        graphicsClone.SetActive (false);
-        // Disable slicing
-        for (int i = 0; i < originalMaterials.Length; i++) {
-            originalMaterials[i].SetVector ("sliceNormal", Vector3.zero);
-        }
-    }
+	// Called once no longer touching portal (excluding when teleporting)
+	public virtual void ExitPortalThreshold()
+	{
+		if (Equals(_graphicsObject, null)) return;
 
-    public void SetSliceOffsetDst (float dst, bool clone) {
-        for (int i = 0; i < originalMaterials.Length; i++) {
-            if (clone) {
-                cloneMaterials[i].SetFloat ("sliceOffsetDst", dst);
-            } else {
-                originalMaterials[i].SetFloat ("sliceOffsetDst", dst);
-            }
+		GraphicsClone.SetActive(false);
+		// Disable slicing
+		for (int i = 0; i < OriginalMaterials.Length; i++)
+		{
+			OriginalMaterials[i].SetVector(SliceNormal, Vector3.zero);
+		}
+	}
 
-        }
-    }
+	public void SetSliceOffsetDst(float dst, bool clone)
+	{
+		for (int i = 0; i < OriginalMaterials.Length; i++)
+		{
+			if (clone)
+			{
+				CloneMaterials[i].SetFloat(SliceOffsetDst, dst);
+			}
+			else
+			{
+				OriginalMaterials[i].SetFloat(SliceOffsetDst, dst);
+			}
+		}
+	}
 
-    Material[] GetMaterials (GameObject g) {
-        var renderers = g.GetComponentsInChildren<MeshRenderer> ();
-        var matList = new List<Material> ();
-        foreach (var renderer in renderers) {
-            foreach (var mat in renderer.materials) {
-                matList.Add (mat);
-            }
-        }
-        return matList.ToArray ();
-    }
+	private static Material[] GetMaterials(GameObject g)
+	{
+		var renderers = g.GetComponentsInChildren<MeshRenderer>();
+		var matList = new List<Material>();
+		foreach (var renderer in renderers)
+		{
+			foreach (var mat in renderer.materials)
+			{
+				matList.Add(mat);
+			}
+		}
+
+		return matList.ToArray();
+	}
 }
