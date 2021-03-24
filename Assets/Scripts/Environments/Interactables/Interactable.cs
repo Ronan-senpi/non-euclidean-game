e6e44c33bfaff7aac0a3fc7924cd7fda09423e3d
Aedrinios <clementclerc2@gmail.com>
2021-03-24 21:43:14 +0100

    add: basic interaction with objects & moving objects

---
+++ Assets/Scripts/Environments/Interactables/Interactable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    [HideInInspector] public UnityEvent onPlayerInteract = new UnityEvent();
+
+    private void Awake()
+    {
+        onPlayerInteract.AddListener(ActionOnUse);    
+    }
+
+
+    protected virtual void ActionOnUse() {
+        Debug.Log("INTERACT");
+    }
+}
---
+++ Assets/Scripts/Environments/Interactables/InteractableMovable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableMovable : Interactable
+{
+    [SerializeField] private Transform takenPosition;
+    private bool taken = false;
+
+    protected override void ActionOnUse()
+    {
+        if (!taken)
+        {
+            PlayerInteraction.Instance.ObjectInHand = this;
+            transform.position = takenPosition.position;
+            transform.SetParent(takenPosition);
+            taken = true;
+        }
+        else
+        {
+            transform.SetParent(null);
+            taken = false;
+        }
+    }
+
+
+}
