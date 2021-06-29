d322ca7e11d7fd77d93b335585f0747fb1518a83
Aedrinios <clementclerc2@gmail.com>
2021-06-29 23:22:54 +0200

    wip tutorial scene + repare door animation

--- Assets/Scripts/Environments/Activable/Door.cs
+++ Assets/Scripts/Environments/Activable/Door.cs
@@ -33,8 +33,8 @@ public class Door : Activable
         {
             if (Vector3.Distance(leftWing.position, rightWing.position) < openingDistance)
             {
-                leftWing.Translate(-Vector3.right * step);
-                rightWing.Translate(Vector3.right * step);
+                leftWing.Translate(-transform.right * step);
+                rightWing.Translate(transform.right * step);
             }
         }
         else
