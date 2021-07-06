1a73fa6fea1091977d5e39c2c6aaac802fce3273
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2021-07-06 11:59:27 +0200

    rdm portals script

--- Assets/RdmPortalController.cs
+++ Assets/RdmPortalController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class RdmPortalController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static RdmPortalController instance;
+    public static RdmPortalController Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<RdmPortalController>();
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    public int nbPassagesBeforeDestination = 3;
+    public int currentPassagesBeforeDestination { get; private set; } = 0;
+
+    [SerializeField]
+    Portal destination;
+
+
+    [SerializeField]
+    List<GameObject> portalsContainers;
+
+    List<GameObject> portals = new List<GameObject>();
+
+    private void Awake()
     {
-        
+        //Init la liste des portails
+        foreach (GameObject item in portalsContainers)
+            for (int i = 0; i < item.transform.childCount; i++)
+                portals.Add(item.transform.GetChild(i).gameObject);
+    }
+
+    public Portal GetPortal(int index)
+    {
+        if (currentPassagesBeforeDestination >= nbPassagesBeforeDestination)
+        {
+            return destination;
+        }
+        if (portals.Count > index)
+        {
+            ++currentPassagesBeforeDestination;
+            Debug.Log("currentPassagesBeforeDestination" + currentPassagesBeforeDestination);
+            return portals[index].GetComponent<Portal>();
+        }
+        return null;
+    }
+
+    public Portal GetRdmPortal()
+    {
+        return this.GetPortal(Random.Range(0, portals.Count));
     }
 }
