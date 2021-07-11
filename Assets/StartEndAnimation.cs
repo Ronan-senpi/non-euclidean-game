5d011763528644bf14acd51f4d359fb7210edd31
Ronan <r.dhersignerie@gmail.com>
2021-07-11 22:11:34 +0200

    Final Ok + Generic

--- Assets/StartEndAnimation.cs
+++ Assets/StartEndAnimation.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartEndAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Generic");
     }
 }
